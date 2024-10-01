4b4bc4ee1c3526fbccbbcca6569a9f63a1404528
Abdullah <abdullah.khamoor4@gmail.com>
2024-10-01 04:03:28 -0700

    secound commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Quiz from './Components/Quiz Component/Quiz'
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
    <>
-   <Routes>
-   <Route path = "//https://AbdullahKhamoor.github.io/Quiz-app" element = {<Quiz />} />
-   </Routes>
-    
+   {/* <Routes>
+   <Route  element = {<Quiz />} />
+   </Routes> */}
+    <Quiz />
    </>
   )
 }
