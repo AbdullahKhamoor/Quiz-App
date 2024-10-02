64be6d77a0aa5983b621b33447f1d364b8b9a22a
Abdullah <abdullah.khamoor4@gmail.com>
2024-10-02 05:29:07 -0700

    responsive quiz App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import Quiz from './Components/Quiz Component/Quiz'
-// import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
    <>
-   {/* <Routes>
-   <Route  element = {<Quiz />} />
-  </Routes> */}
+   
     <Quiz />
    </>
   )
